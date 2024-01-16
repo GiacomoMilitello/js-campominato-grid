4366eb002d25ebd32bfcf845d535f0d049f016d8
GiacomoMilitello <g.militello95@gmail.com>
2024-01-16 15:32:36 +0100

    griglia

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,11 @@ Aggiungere una select accanto al bottone di generazione, che fornisca una scelta
 - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
 */
+
+const ul = document.querySelector('ul.list');
+        for (let i = 1; i <= 100; i++) {
+            const element = document.createElement('li');
+                element.innerText = i;
+                element.classList.add('bg-body-secondary', 'ratio', 'd-flex', 'justify-content-center', 'align-items-center', 'fw-bold', 'border', 'border-white', 'border-2');
+            ul.appendChild(element);
+        }
